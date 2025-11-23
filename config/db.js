2f75b2929c62c906494d3a93c0ec42f7cfbdcc71
issazambrano55 <issazambrano55@gmail.com>
2025-11-23 14:13:06 -0600

    Correccion de errores

--- config/db.js
+++ config/db.js
@@ -4,25 +4,22 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const db = await mysql.createPool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
-// Probar conexión
-(async () => {
-  try {
-    const connection = await db.getConnection();
-    console.log("Conectado a MySQL correctamente");
-    connection.release();
-  } catch (error) {
-    console.error("Error conectando a MySQL:", error);
-  }
-})();
+
+db.getConnection()
+  .then((conn) => {
+    console.log(" Conexión a MySQL exitosa");
+    conn.release();
+  })
+  .catch((err) => {
+    console.error(" Error conectando a MySQL:", err.message);
+  });
 
 export default db;
+
