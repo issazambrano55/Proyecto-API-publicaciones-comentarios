34342d4b6f4b2d3e35ed19c0d2617f7220b67abd
issazambrano55 <issazambrano55@gmail.com>
2025-11-26 00:02:22 -0600

    Correccion de errores mensaje correcto

--- middlewares/validarComentarios.js
+++ middlewares/validarComentarios.js
@@ -3,13 +3,15 @@ import { comentarioSchema } from "../schemas/comentario_schema.js";
 export const validarComentarios = (req, res, next) => {
   const parseResult = comentarioSchema.safeParse(req.body);
 
-  if (!parseResult.success) {
-    const errores = parseResult.error.issues.map((e) => ({
+  if (!parseResult.success) { const errores = parseResult.error.issues.map((e) => ({
       campo: e.path.join("."),
       mensaje: e.message
     }));
 
-   
+   return res.status(400).json({ success: false, message: "Error de validación en el comentario",
+      errors: errores
+    });
+  }
 
   req.validated = parseResult.data;
   next();
