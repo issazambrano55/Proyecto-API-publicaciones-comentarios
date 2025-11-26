8f0922de6448df61238330ef90cb98b625c89930
issazambrano55 <issazambrano55@gmail.com>
2025-11-25 18:08:11 -0600

    implementación de rutas en el server.js

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import db from "./config/db.js";
 import authRoutes from "./routes/auth_routes.js";
+import comentarioRoutes from "./routes/comentario_routes.js";
+import publicacion_routes from "./routes/publicacion_routes.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
+
+
 dotenv.config();
 
 const app = express();
 
-
-app.use(express.json());
 app.use(cors({
   origin: [
     'http://localhost:3000',
@@ -18,24 +21,21 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-app.use("/api/auth", authRoutes);
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API de comentarios funcionando");
 });
 
+app.use("/api/auth", authRoutes);
+app.use("/api/publicaciones", publicacionRoutes);
+app.use("/api", comentarioRoutes);
 
-app.get("/api/test-db", async (req, res) => {
-  try {
-    const connection = await db.getConnection();
-    connection.release();
-    res.send("Conexión a MySQL funcionando correctamente");
-  } catch (error) {
-    res.status(500).send("Error al conectar a MySQL: " + error.message);
-  }
-});
 
-const PORT = process.env.PORT || 3000;
+app.use(errorHandler);  
+
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
